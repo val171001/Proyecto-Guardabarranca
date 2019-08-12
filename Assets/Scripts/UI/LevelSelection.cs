44e000634ea6c8ba4bc24cde34b8dbdd3917671e
Luis Diego Fernandez <luisdiegofv97@gmail.com>
2019-08-12 15:07:35 -0600

    Creacion seleccion de niveles
    Se creo la escena para cambio de niveles, con sus respectivas animaciones.

---
+++ Assets/Scripts/UI/LevelSelection.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelection : MonoBehaviour
+{
+    private Animator ani;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ani.SetBool("ChangeRight", false);
+        ani.SetBool("ChangeLeft", false);
+    }
+
+    public void ChangeRight()
+    {
+        ani.SetBool("ChangeRight", true);
+        ani.SetBool("ChangeLeft", false);
+    }
+
+    public void ChangeLeft()
+    {
+        ani.SetBool("ChangeLeft", true);
+        ani.SetBool("ChangeRight", false);
+    }
+
+    public void ChangeLevel(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
