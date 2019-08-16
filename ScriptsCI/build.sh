f8fd6c45e771c41204b346448800675bcf53d81f
Josue Valenzuela <val171001@uvg.edu.gt>
2019-08-15 20:30:31 -0600

    Install and Build

--- ScriptsCI/build.sh
+++ ScriptsCI/build.sh
@@ -1,3 +1,27 @@
 #! /bin/sh
 
-echo 'Running tests'
+echo "Running playmode tests for ${PROJECT_NAME}"
+
+# Run unity tests on playmode
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+	-batchmode \
+	-nographics \
+	-silent-crashes \
+	-logFile $(pwd)/Logs/PlayModeTests.log \
+	-projectPath "$(pwd)" \
+	-runTests \
+    -testPlatform playmode
+	-editorTestsResultFile $(pwd)/Logs/TestResults.xml \
+	-quit
+
+
+TEST_SUCCESS=$?
+echo "Test results"
+cat $(pwd)/Logs/TestResults.xml
+# Abort on failed tests
+if [ $TEST_SUCCESS -ne 0 ]; then
+    echo "Tests failed"
+    exit $TEST_SUCCESS
+fi
+
+# Build APK
--- ScriptsCI/install.sh
+++ ScriptsCI/install.sh
@@ -1,15 +1,34 @@
 #! /bin/sh
 
+funciton after_task() {
+    if [ $1 -ne 0 ]; then
+        echo $2
+        exit $1
+    fi
+}
+
 # Install homebrew
+echo "Installing Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+after_task $? "Homebrew downlad failed"
 
 # Install aira2
+echo "Installing aria2"
 brew install aria2
+after_task $? "Aria2 install failed"
 
 # Dowload unity torrent
-curl --output Unity.torrent https://download.unity3d.com/download_unity/ca4d5af0be6f/Unity-2019.2.1f1.torrent
-echo $(ls)
+echo "Dowloading Unity Torrent"
+curl --output Unity.torrent $UNITY_TORRENT_URL
+after_task $? "Unity torrent downlad failed"
 
 # Dowload macos unity files using torrent and aria2
-aria2c --select-file=3,4 Unity.torrent
-echo $(ls)
+echo "Downloading Unity with andorid support"
+aria2c --seed-time=0 --select-file=3,4 Unity.torrent
+after_task $? "Unity downlad failed"
+
+# Install unity and add android support
+echo "Installing unity"
+sudo installer -dumplog -package ./Unity-2019.2.1f1/MacOSX/Unity.pkg -target /
+echo "Adding android support"
+sudo installer -dumplog -package ./Unity-2019.2.1f1/MacOSX/UnitySetup-Android-Support-for-Editor.pkg -target /
