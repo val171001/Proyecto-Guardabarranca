bc46395a1088820c19f31284f8dbd2f1dd971daa
Josue Valenzuela <val171001@uvg.edu.gt>
2019-08-15 18:02:59 -0600

    install.sh

--- ScriptsCI/install.sh
+++ ScriptsCI/install.sh
@@ -1,3 +1,15 @@
 #! /bin/sh
 
-echo 'Installing unity'
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install aira2
+brew install aria2
+
+# Dowload unity torrent
+curl --output Unity.torrent https://download.unity3d.com/download_unity/ca4d5af0be6f/Unity-2019.2.1f1.torrent
+echo $(ls)
+
+# Dowload unity files using torrent and aria2
+aria2c Unity.torrent
+echo $(ls)
