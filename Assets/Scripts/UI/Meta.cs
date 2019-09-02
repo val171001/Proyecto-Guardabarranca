961de842cacae4973ef202669cfe92e9c1e0bf17
Mac17369 <Mac17369@uvg.edu.gt>
2019-09-01 22:34:56 -0600

    Cambios Terreno

--- Assets/Scripts/UI/Meta.cs
+++ Assets/Scripts/UI/Meta.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Meta : MonoBehaviour
 {
@@ -13,6 +14,14 @@ public class Meta : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Flag")
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }
