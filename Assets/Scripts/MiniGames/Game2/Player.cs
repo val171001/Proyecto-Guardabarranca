3cf33da8074ed6c2cc98fb36cb1bb3228149deb3
Josue Valenzuela <val171001@uvg.edu.gt>
2019-10-21 19:11:40 -0600

    Add player

---
+++ Assets/Scripts/MiniGames/Game2/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public Vector2 jumpForce = new Vector2(0, 300);
+
+	void Update()
+	{
+		if (Input.GetKeyUp("space"))
+		{
+			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+			GetComponent<Rigidbody2D>().AddForce(jumpForce);
+		}
+	}
+}
