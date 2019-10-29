ffbfaae57f3bdf438d555c3d7d52041f186a48b9
Josue Valenzuela <josue@kemik.gt>
2019-10-29 11:47:58 -0600

    add username

--- ci/test.sh
+++ ci/test.sh
@@ -10,6 +10,8 @@ ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x2
   -testPlatform $TEST_PLATFORM \
   -testResults $(pwd)/$TEST_PLATFORM-results.xml \
   -logFile /dev/stdout \
+  -username "$UNITY_USERNAME" \
+  -password "$UNITY_PASSWORD" \
   -batchmode
 
 UNITY_EXIT_CODE=$?
