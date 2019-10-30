b4ff97757b321f5c0ceeff692b950e903899037b
Josue Valenzuela <josue@kemik.gt>
2019-10-30 06:58:59 -0600

    Try removing command

--- ci/build.sh
+++ ci/build.sh
@@ -16,8 +16,8 @@ ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x2
   -customBuildTarget $BUILD_TARGET \
   -customBuildName $BUILD_NAME \
   -customBuildPath $BUILD_PATH \
-  -executeMethod BuildCommand.PerformBuild \
-  -logFile /dev/stdout
+  #-executeMethod BuildCommand.PerformBuild \
+  #-logFile /dev/stdout
 
 UNITY_EXIT_CODE=$?
 
