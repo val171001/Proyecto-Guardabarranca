c5dd9c219b5298e57ac624373c3f145314eb447f
David Valenzuela <33351729+val171001@users.noreply.github.com>
2019-10-29 21:07:13 -0600

    Update docker_build.sh

--- ci/docker_build.sh
+++ ci/docker_build.sh
@@ -4,10 +4,10 @@ set -e
 
 docker run \
   -e BUILD_NAME \
-  -e $UNITY_LICENSE_CONTENT \
-  -e $BUILD_TARGET \
-  -e $UNITY_USERNAME \
-  -e $UNITY_PASSWORD \
+  -e UNITY_LICENSE_CONTENT \
+  -e BUILD_TARGET \
+  -e UNITY_USERNAME \
+  -e UNITY_PASSWORD \
   -w /project/ \
   -v $(pwd):/project/ \
   $IMAGE_NAME \
